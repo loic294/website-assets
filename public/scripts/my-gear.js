415d7a0ef8f4ae2da70417313855e490628b7bb5
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2024-05-28 02:53:16 +0000

    Add tag name

--- public/scripts/my-gear.js
+++ public/scripts/my-gear.js
@@ -4,3 +4,10 @@ if (window.location.pathname.includes('/my-gear/category/')) {
   elem.style.display = 'block';
   elem.innerText = decodeURI(category);
 } 
+
+if (window.location.pathname.includes('/my-gear/tag/')) {
+    const category = window.location.pathname.replace('/my-gear/tag/', '').replaceAll('+', ' ');
+    const elem = document.getElementById('cateogry-title');
+    elem.style.display = 'block';
+    elem.innerText = decodeURI(category);
+  } 
