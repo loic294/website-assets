177fc04a86925b0cfac7e47602eff486f3134da0
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2024-10-05 11:28:28 -0400

    Fix language and menu style

--- public/scripts/languages.js
+++ public/scripts/languages.js
@@ -1,11 +1,6 @@
-const pickers = document.getElementsByClassName('language-picker-content');
-
-console.log('PICKERS', pickers);
-
-for (const picker of pickers) {
-    console.log('Pick', picker.childNodes, picker.childNodes[1].firstChild)
-    const aElem = picker.childNodes[1].firstChild;
-    if (aElem) {
-        aElem.href = 'https://loicbellemarealford.ca';
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    Weglot.on("languageChanged", function(newLang, prevLang) {
+        console.log("The language on the page just changed to (code): " + newLang)
+        console.log("The full name of the language is: " + Weglot.getLanguageName(newLang))
+    })
+});
