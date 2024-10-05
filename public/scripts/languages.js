619b639c8b3dbfd01050117de4bf190d6d1acdb7
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2024-10-05 10:20:34 -0400

    Add logging

--- public/scripts/languages.js
+++ public/scripts/languages.js
@@ -1,6 +1,9 @@
 const pickers = document.getElementsByClassName('language-picker-content');
 
+console.log('PICKERS', pickers);
+
 for (const picker of pickers) {
+    console.log('Pick', picker.childNodes, picker.childNodes[1].firstChild)
     const aElem = picker.childNodes[1].firstChild;
     if (aElem) {
         aElem.href = 'https://loicbellemarealford.ca';
