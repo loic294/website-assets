af99927c8275dbf95680beb39850d5a792357445
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2024-10-05 10:18:41 -0400

    Fix script

--- public/scripts/languages.js
+++ public/scripts/languages.js
@@ -2,7 +2,7 @@ const pickers = document.getElementsByClassName('language-picker-content');
 
 for (const picker of pickers) {
     const aElem = picker.childNodes[1].firstChild;
-    if (aElem) 
+    if (aElem) {
         aElem.href = 'https://loicbellemarealford.ca';
     }
 }
